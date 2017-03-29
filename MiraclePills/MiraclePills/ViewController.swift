3ea3b8c9a8d57c76c939d27bb6cb9db7626dcac1
HoangPhan <hoangphan@HoangPhans-MBP.net.fpt>
2017-03-29 13:08:08 +0700

    ...

---
+++ MiraclePills/MiraclePills/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MiraclePills
+//
+//  Created by HoangPhan on 3/29/17.
+//  Copyright © 2017 HoangPhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+
+}
+
